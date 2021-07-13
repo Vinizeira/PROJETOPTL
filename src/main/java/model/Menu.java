3fc190a001db2b6399bcb9ccb33042e0f2c50a2b
Vinizeira <42251547+Vinizeira@users.noreply.github.com>
2021-07-13 15:06:19 -0300

    Create Menu.java

---
+++ src/main/java/model/Menu.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author viniz
+ */
+public class Menu {
+    
+   private int idMenu;
+   private String nome;
+   private String icone;
+   private String link;
+   private int exibir;
+
+    public Menu() {
+       
+    }
+
+    public Menu(int idMenu, String nome, String icone, String link, int exibir) {
+        this.idMenu = idMenu;
+        this.nome = nome;
+        this.icone = icone;
+        this.link = link;
+        this.exibir = exibir;
+    }
+
+    public int getIdMenu() {
+        return idMenu;
+    }
+
+    public void setIdMenu(int idMenu) {
+        this.idMenu = idMenu;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIcone() {
+        return icone;
+    }
+
+    public void setIcone(String icone) {
+        this.icone = icone;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public int getExibir() {
+        return exibir;
+    }
+
+    public void setExibir(int exibir) {
+        this.exibir = exibir;
+    }
+
+    @Override
+    public String toString() {
+        return "Menu{" + "nome=" + nome + '}';
+    }
+
+ 
+    
+    
+    
+        
+    
+   
+    
+}
